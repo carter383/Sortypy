e8d0a882b1a486001a1ece7b91b05675180e2960
carter383 <80467899+carter383@users.noreply.github.com>
2021-12-22 15:58:00 +0000

    Working Code! Ready for release

--- Sortpy.py
+++ Sortpy.py
@@ -9,147 +9,158 @@ from watchdog.events import FileSystemEventHandler
 
 
 def loadJsons():
-    with open('Config.json') as c:
+    with open("Config.json") as c:
         Config = json.load(c)
-    with open('Rules.json') as r:
+    with open("Rules.json") as r:
         Rules = json.load(r)
 
     return Config, Rules
 
 config, rules = loadJsons()
+
+def log(msg):
+    logfile = "SortyPyLog.txt"
+    with open(logfile, "a") as f:
+        f.writelines('\n')
+        f.writelines(str(msg))
+
 def files():
     files = [ ]
-    files = os.listdir(config['Config']['Source_Folder'])
+    files = os.listdir(config["Config"]["Source_Folder"])
     return files
 
 def Copy(src, dest):
     if os.path.isfile(dest)!= True:
         shutil.copy(src, dest)
-    files()
+        log("Copyed: "+str(src)+ " to " + str(dest))
+    
 def Move(src, dest,rename,replace):
     if rename == True:
-        destsplit = dest.split('.')
+        destsplit = dest.split(".")
         i=1
-        while os.path.isfile(destsplit[0]+'_'+str(i)+destsplit[1]) == True:
+        while os.path.isfile(destsplit[0]+"_"+str(i)+destsplit[1]) == True:
             i+=1
-        newdest = destsplit[0]+'_'+str(i)+destsplit[1]
+        newdest = destsplit[0]+"_"+str(i)+destsplit[1]
         shutil.move(src,newdest)
+        log("Moved and Renamed: "+ str(src)+ " to ", str(newdest))
     else:
         if os.path.isfile(dest)==True:
             if replace == True:
                 shutil.move(src,dest)
+                log("File moved and Overwritten Existing File: "+ str(src)+ " to " +str(dest))
             else:
-                print('file exists')
+                log("File exists in Destination unable to move: " + str(src))
         else:
             shutil.move(src,dest)
+            log("File moved: "+ str(src)+ " to " +str(dest))
     files()
 
 def partial_name(file, rule):
-    src = config['Config']['Source_Folder']+'/'+file
-    dest = rule['Destination']+'/'+file
-    match = rule['Rule_Params']
+    src = config["Config"]["Source_Folder"]+"/"+file
+    dest = rule["Destination"]+"/"+file
+    match = rule["Rule_Params"]
     if len(match) ==1:
        isin = match in file 
        if isin == True:
-            if rule['Rule_exec']== 'Move':
-               Move(src,dest,rule['Rename'],rule['Replace'])
-            elif rule['Rule_exec']== 'Copy':
+            if rule["Rule_exec"]== "Move":
+               Move(src,dest,rule["Rename"],rule["Replace"])
+            elif rule["Rule_exec"]== "Copy":
                Copy(src,dest)
     elif len(match) >1:
         for i in range(0,len(match)):
             isin = match[i] in file 
             if isin == True:
-                if rule['Rule_exec']== 'Move':
-                    Move(src,dest,rule['Rename'],rule['Replace'])
-                elif rule['Rule_exec']== 'Copy':
+                if rule["Rule_exec"]== "Move":
+                    Move(src,dest,rule["Rename"],rule["Replace"])
+                elif rule["Rule_exec"]== "Copy":
                     Copy(src,dest)
     else:
-        print('error')
+        log("error partial name Rule_Params")
     
 
 
 def exact_name(file,rule):
-    src = config['Config']['Source_Folder']+'/'+file
-    dest = rule['Destination']+'/'+file
-    match = rule['Rule_Params']
+    src = config["Config"]["Source_Folder"]+"/"+file
+    dest = rule["Destination"]+"/"+file
+    match = rule["Rule_Params"]
     if len(match) ==1:
          if file == match:
-            if rule['Rule_exec']== 'Move':
-                Move(src,dest,rule['Rename'],rule['Replace'])
-            elif rule['Rule_exec']== 'Copy':
+            if rule["Rule_exec"]== "Move":
+                Move(src,dest,rule["Rename"],rule["Replace"])
+            elif rule["Rule_exec"]== "Copy":
                 Copy(src,dest)
     elif len(match) >1:
         for i in range(0,len(match)):
             if file == match[i]:
-                if rule['Rule_exec']== 'Move':
-                    Move(src,dest,rule['Rename'],rule['Replace'])
-                elif rule['Rule_exec']== 'Copy':
+                if rule["Rule_exec"]== "Move":
+                    Move(src,dest,rule["Rename"],rule["Replace"])
+                elif rule["Rule_exec"]== "Copy":
                     Copy(src,dest)
     else:
-        print('error')
+        log("error exact name Rule_Params")
 
 def extention(file,rule):
-    src = config['Config']['Source_Folder']+'/'+file
-    dest = rule['Destination']+'/'+file
-    match = rule['Rule_Params']
+    src = config["Config"]["Source_Folder"]+"/"+file
+    dest = rule["Destination"]+"/"+file
+    match = rule["Rule_Params"]
     if len(match) ==1:
         isin = str(match) in str(file)
         if isin == True:
-            if rule['Rule_exec']== 'Move':
-                    Move(src,dest,rule['Rename'],rule['Replace'])
-            elif rule['Rule_exec']== 'Copy':
+            if rule["Rule_exec"]== "Move":
+                    Move(src,dest,rule["Rename"],rule["Replace"])
+            elif rule["Rule_exec"]== "Copy":
                     Copy(src,dest)
     elif len(match) >1:
         for i in range(0,len(match)):
             isin = str(match[i]) in str(file)
             if isin == True:
-                if rule['Rule_exec']== 'Move':
-                    Move(src,dest,rule['Rename'],rule['Replace'])
-                elif rule['Rule_exec']== 'Copy':
+                if rule["Rule_exec"]== "Move":
+                    Move(src,dest,rule["Rename"],rule["Replace"])
+                elif rule["Rule_exec"]== "Copy":
                     Copy(src,dest)
     else:
-        print('error')
+        log("error extentions Rule_Params")
 
 def RuleCheck(file,rule):
-    ruleType = rule['Rule_Type']
-    if ruleType == 'extention':
+    ruleType = rule["Rule_Type"]
+    if ruleType == "extention":
         extention(file,rule)
-    elif ruleType == 'partial':
+    elif ruleType == "partial":
         partial_name(file,rule)
-    elif ruleType == 'exact':
+    elif ruleType == "exact":
         exact_name(file,rule)
     else:
-        print('invalid rule type')
+       log("invalid rule type: " + str(rule))
 
 def Rules(file):
-    if len(rules['Priority_Rules']) ==0:
-        print('no rules')
-    elif len(rules['Priority_Rules'])!=1:
+    if len(rules["Rules"]) ==0:
+        log("no rules")
+    elif len(rules["Rules"])!=1:
             
-        for i in range(1,len(rules['Rules'])+1):
-            rule = rules['Rules']['Rule-'+str(i)]
-            if rule['Active'] == True:
-                ruleType = rule['Rule_Type']
+        for i in range(1,len(rules["Rules"])+1):
+            rule = rules["Rules"]["Rule-"+str(i)]
+            if rule["Active"] == True:
+                ruleType = rule["Rule_Type"]
                 RuleCheck(file,rule)
-    elif len(rules['Priority_Rules'])==1:
-            rule = rules['Rules']['Rule-1']
-            if rule['Active'] == True:
-                ruleType = rule['Rule_Type']
+    elif len(rules["Priority_Rules"])==1:
+            rule = rules["Rules"]["Rule-1"]
+            if rule["Active"] == True:
+                ruleType = rule["Rule_Type"]
                 RuleCheck(file,rule)
 
 def Priority_Rules(file):
-    if len(rules['Priority_Rules']) ==0:
-        print('no rules')
-    elif len(rules['Priority_Rules'])!=1:
-        for i in range(1,len(rules['Priority_Rules'])+1):
-            rule = rules['Priority_Rules']['Rule-p-'+str(i)]
-            if rule['Active'] == True:
-                ruleType = rule['Rule_Type']
+    if len(rules["Priority_Rules"]) ==0:
+        log("no  Priroity rules")
+    elif len(rules["Priority_Rules"])!=1:
+        for i in range(1,len(rules["Priority_Rules"])+1):
+            rule = rules["Priority_Rules"]["Rule-p-"+str(i)]
+            if rule["Active"] == True:
+                ruleType = rule["Rule_Type"]
                 RuleCheck(file,rule)
-    elif len(rules['Priority_Rules'])==1:
-            rule = rules['Priority_Rules']['Rule-p-1']
-            if rule['Active'] == True:
-                ruleType = rule['Rule_Type']
+    elif len(rules["Priority_Rules"])==1:
+            rule = rules["Priority_Rules"]["Rule-p-1"]
+            if rule["Active"] == True:
+                ruleType = rule["Rule_Type"]
                 RuleCheck(file,rule)
 
             
@@ -158,16 +169,19 @@ def Run_Rules():
 
     newFiles = files()
     print (newFiles)
+    priorityLen = len(rules["Priority_Rules"])
     if len(newFiles)!= 1:
         filesLen =len(newFiles)
         for i in range(0,filesLen):
-            Priority_Rules(newFiles[i])
+            if priorityLen != 0:
+                Priority_Rules(newFiles[i])
             checkfile = files()
             checkfilebool = newFiles[i] in checkfile
             if checkfilebool == True:
                 Rules(newFiles[i])
     else:
-        Priority_Rules(newFiles)
+        if priorityLen != 0:
+             Priority_Rules(newFiles)
         checkfile = files()
         checkfilebool = newFiles in checkfile
         if checkfilebool == True:
@@ -175,7 +189,7 @@ def Run_Rules():
         Rules(newFiles)
 
 class Watcher:
-    DIRECTORY_TO_WATCH = config['Config']['Source_Folder']
+    DIRECTORY_TO_WATCH = config["Config"]["Source_Folder"]
 
     def __init__(self):
         self.observer = Observer()
@@ -189,7 +203,7 @@ class Watcher:
                 time.sleep(5)
         except:
             self.observer.stop()
-            print("Error")
+            log("Error watcher")
         self.observer.join()
 
 
@@ -200,22 +214,24 @@ class Handler(FileSystemEventHandler):
         if event.is_directory:
             return None
 
-        elif event.event_type == 'created':
+        elif event.event_type == "created":
             # Take any action here when a file is first created.
-            print ("Received created event - %s." % event.src_path)
+            log("Received created event - %s." % event.src_path)
             time.sleep(5)
             config, rules = loadJsons()
             time.sleep(5)
             Run_Rules()
 
-        elif event.event_type == 'modified':
+        elif event.event_type == "modified":
             # Taken any action here when a file is modified.
-            print ("Received modified event - %s." % event.src_path)
+            log("Received modified event - %s." % event.src_path)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    log("Starting")
     w = Watcher()
     w.run()
+    log("Running")
 
 
 
